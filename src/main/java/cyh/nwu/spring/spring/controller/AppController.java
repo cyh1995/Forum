13888ece0c27c1f071fb7ca9120731077d17f7b6
cyh1995 <969963179@qq.com>
2019-07-10 16:27:47 +0800

    add index and bootstrap

--- src/main/java/cyh/nwu/spring/spring/controller/AppController.java
+++ src/main/java/cyh/nwu/spring/spring/controller/AppController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
-@GetMapping("/hello")
-    public String hello(@RequestParam(name = "name",required = false ,defaultValue = "World")String name, Model model){
-    model.addAttribute("name",name);
-    return "hello";
+@GetMapping("/")
+    public String index(){
+    return "index";
 }
 }
+
